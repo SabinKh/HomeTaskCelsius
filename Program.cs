cb6a8d3b02b7e22e59da44d550bf8d647630c471
SabinKh <91114252+SabinKh@users.noreply.github.com>
2021-10-18 01:33:26 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Celsius_hometask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kelvin kelvin = new Kelvin(500);
+            Celsius obj1 = kelvin;
+            Console.WriteLine(obj1.Degree);
+            
+        }
+    }
+    class Celsius
+    {
+        public double Degree { get; set; }
+        public Celsius(double c)
+        {
+            Degree = c;
+        }
+        
+    }
+    class Kelvin
+    {
+        public double Degree { get; set; }
+        public Kelvin(double k)
+        {
+            Degree = k;
+        }
+        public static implicit operator Celsius(Kelvin kk)
+        {
+            return new Celsius(kk.Degree - 273.15);
+        }
+    }
+}
